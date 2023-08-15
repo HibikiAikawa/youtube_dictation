ccc94feb11e91f3c5007c9e82a0867371ffc35b4
HibikiAikawa <hibikingxfin@gmail.com>
2023-08-15 04:06:45 +0000

    fix code
    git rm --cached *.pyc and add .gitignore

--- bin/run.sh
+++ bin/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm -it -v ${PWD}:/work -w /work -p 8888:8000 --name youtube_dictation youtube_dictation_image $1
+docker run -it -v ${PWD}:/work -w /work -p 8888:8000 --name youtube_dictation youtube_dictation_image $1
