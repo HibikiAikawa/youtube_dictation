f9f3fbef72b5f11c27e97ee605d4b28765d7dc77
HibikiAikawa <hibikingxfin@gmail.com>
2023-08-17 08:37:07 +0000

    update question creation
    [Music]などの[]系の字幕は問題に反映されないように修正

--- app/views.py
+++ app/views.py
@@ -15,13 +15,22 @@ def get_question(request):
     question = Question.objects.order_by('?').first()
     transcript = YouTubeTranscriptApi.list_transcripts(question.youtube_id)
     transcript_list = next(iter(transcript)).fetch()
-    idx = np.random.randint(1, len(transcript_list)-2)
 
-    start_time = transcript_list[idx]['start']
-    pre_sentence = transcript_list[idx]['text']
-    sentence = transcript_list[idx+1]['text']
-    post_sentence = transcript_list[idx+2]['text']
+    def get_random_idx(_transcript_list):
+        idx = np.random.randint(1, len(_transcript_list)-2)
+        start_time = transcript_list[idx]['start']
+        pre_sentence = transcript_list[idx]['text']
+        sentence = transcript_list[idx+1]['text']
+        post_sentence = transcript_list[idx+2]['text']
+        return start_time, pre_sentence, sentence, post_sentence, idx
 
+    def pick_question(_transcript_list):
+        start_time, pre_sentence, sentence, post_sentence, idx = get_random_idx(_transcript_list)
+        if '[' in pre_sentence + sentence + post_sentence:
+            pick_question()
+        return start_time, pre_sentence, sentence, post_sentence, idx
+
+    start_time, pre_sentence, sentence, post_sentence, idx = pick_question(transcript_list)
     question.start_time = int(start_time)
     question.pre_sentence = pre_sentence
     question.sentence = sentence
